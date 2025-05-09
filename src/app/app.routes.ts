b997d28276e64925c3bf6edc5159c654c361ae15
Shanhun-Codes <shanhun.codes@gmail.com>
2025-05-08 22:01:57 -0500

    set initial structure for signin page. am going to use reactive forms from angular.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,19 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LandingComponent } from './pages/landing/landing.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
 
 export const routes: Routes = [
-    {
-        path: '', component: LandingComponent
-    },
-    {
-        path: 'dashboard', component: DashboardComponent, children: []
-    }
+  {
+    path: '',
+    component: LandingComponent,
+  },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'sign-in', component: SignInComponent },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    children: [],
+  },
 ];
